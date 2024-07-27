d31cbcf538b7106b6de7273e03e2867fc080db60
Flors Rueda <ruedaflors@gmail.com>
2024-07-27 13:49:30 +0200

    fix babel file #1

--- proof-of-concept/babel.config.js
+++ proof-of-concept/babel.config.js
@@ -1,16 +1,19 @@
-api.cache(false)
-module.exports = {
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        envName: 'APP_ENV',
-        moduleName: '@env',
-        path: '.env',
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
     ],
-  ],
+  };
 };
