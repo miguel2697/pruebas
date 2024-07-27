ae0a8bb067fe1fbf60cfe4d4dc6f1d942b2b5bde
Flors Rueda <ruedaflors@gmail.com>
2024-07-27 17:42:36 +0200

    add message on touch #13

--- proof-of-concept/src/components/LateralScroll.jsx
+++ proof-of-concept/src/components/LateralScroll.jsx
@@ -1,10 +1,16 @@
-import { View, Text, Dimensions, ScrollView } from 'react-native';
+import React from 'react';
+import { View, Text, Dimensions, ScrollView, TouchableOpacity, Alert } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
 export default function LateralScroll() {
     const screenPadding = 20;
     const stepFormWidth = width - screenPadding * 2;
+
+    const showAlert = (message) => {
+        Alert.alert(message);
+    };
+
     return (
         <View
             style={{
@@ -25,7 +31,37 @@ export default function LateralScroll() {
                 }}
                 horizontal
             >
-                <View
+                <TouchableOpacity
+                    style={{
+                        width: stepFormWidth,
+                        backgroundColor: "black",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingVertical: 160,
+                    }}
+                    onPress={() => showAlert('Hello!')}
+                    activeOpacity={1}
+                >
+                    <Text style={{ color: "white", fontSize: 20 }}>
+                        Hello!
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{
+                        width: stepFormWidth,
+                        backgroundColor: "orangered",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingVertical: 160,
+                    }}
+                    onPress={() => showAlert('Word 2')}
+                    activeOpacity={1}
+                >
+                    <Text style={{ color: "white", fontSize: 20 }}>
+                        Word 2
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
                     style={{
                         width: stepFormWidth,
                         backgroundColor: "gray",
@@ -33,52 +69,28 @@ export default function LateralScroll() {
                         justifyContent: "center",
                         paddingVertical: 160,
                     }}
-                >
-                    <Text style={{ color: "white", fontSize: 20 }}>
-                        Hello!
-                    </Text>
-                </View>
-                <View
-                    style={{
-                        width: stepFormWidth,
-                        backgroundColor: "green",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        paddingVertical: 160,
-                    }}
-                >
-                    <Text style={{ color: "white", fontSize: 20 }}>
-                        Word 2
-                    </Text>
-                </View>
-
-                <View
-                    style={{
-                        width: stepFormWidth,
-                        backgroundColor: "orange",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        paddingVertical: 160,
-                    }}
+                    onPress={() => showAlert('3 image ^^')}
+                    activeOpacity={1}
                 >
                     <Text style={{ color: "white", fontSize: 20 }}>
                         3 image ^^
                     </Text>
-                </View>
-
-                <View
+                </TouchableOpacity>
+                <TouchableOpacity
                     style={{
                         width: stepFormWidth,
-                        backgroundColor: "red",
+                        backgroundColor: "forestgreen",
                         alignItems: "center",
                         justifyContent: "center",
                         paddingVertical: 160,
                     }}
+                    onPress={() => showAlert('Last one')}
+                    activeOpacity={1}
                 >
                     <Text style={{ color: "white", fontSize: 20 }}>
                         Last one
                     </Text>
-                </View>
+                </TouchableOpacity>
             </ScrollView>
         </View>
     );
