6f605abd7fdd42cbbfd19ad0b4c5211b44a7bd34
Miguel Arias <miguelariasgarau@gmail.com>
2024-07-30 17:52:56 +0200

    update App.js #4

--- proof-of-concept/App.js
+++ proof-of-concept/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola Isa y Miguel!!</Text>
-      <Text>:)</Text>
+      <Text>:D</Text>
 
       <NotificationButton />
 
