2775ff42509f28ce09b62b065a14207d01971a01
Flors Rueda <ruedaflors@gmail.com>
2024-08-10 20:34:57 +0200

    refactor test component #28

--- proof-of-concept/src/components/TestComponent.jsx
+++ proof-of-concept/src/components/TestComponent.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 
+
 export default function TestComponent() {
     const [name, setUser] = useState('');
     const [show, setShow] = useState(false);
