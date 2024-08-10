1efffd493609e71a8de78ff71140737f4e5490ca
Flors Rueda <ruedaflors@gmail.com>
2024-08-10 19:42:09 +0200

    remove log on test component #28

--- proof-of-concept/src/components/TestComponent.jsx
+++ proof-of-concept/src/components/TestComponent.jsx
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 
 export default function TestComponent() {
     const [name, setUser] = useState('');
     const [show, setShow] = useState(false);
-
-    useEffect(() => {
-        console.log('mounting test component')
-    }, [])
 
     return (
         <View>
